96806c8f47c27418545830defb6ba54aed3cb7ec
meissa10 <56763829+meissa10@users.noreply.github.com>
2025-11-17 20:01:52 +0000

    testing login 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     setStatus(''); // Clear previous status
 
     // Check for the sample account
-    if (email === 'user@example.com' && password === 'password123') {
+    if ('@' in email && '.com' in email) {
       
       setStatus('Login successful! Redirecting...');
       
